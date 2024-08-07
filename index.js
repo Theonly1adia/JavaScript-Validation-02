956711cb4eeec7e2a0cbe242710628b461250ce3
Adia Holt <adia.holt@gmail.com>
2024-08-06 20:28:46 -0500

    day2 js

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log ("hello from index.js")
+
+let myName = "Adia";
+
+let lengthofName = myName.length;
+let firstLetterOfName = 
+
+console.log(lengthofName);
+
+if (lengthofName > 7 ) {
+    console.log("You will marry late in life.")
+}
+
+if (lengthofName < 5){
+    console.log("You marry within a year.")
+}
+
+if (myName.length >= 5 && myName.length <= 7) {
+    console.log("You will encounter a once-in-a lifetime opportunity");
+  }
+
+if (myName.charAt(0) === 'R') {
+    console.log("You will be rich!");
+  }
+
+if (myName.includes('i')) {
+    console.log ("You will fall in love this week")
+  }
+
+  
